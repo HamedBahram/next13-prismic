9cbcc60a8b31c8e1aa6a128e4f46c04cffed6928
Hamed Bahram <hamed.bahram@gmail.com>
2023-10-15 11:33:35 -0400

    Add prismic

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+
+import { revalidateTag } from 'next/cache'
+
+export async function POST() {
+  revalidateTag('prismic')
+
+  return NextResponse.json({ revalidated: true, now: Date.now() })
+}
